73c98fbb0c1b6e097cb01843d229db4537341346
rhaynerrs <rhayner3301@gmail.com>
2024-10-30 21:57:38 -0300

    corrige utilizacao das rotas e arquivo index

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import index from './app/index'; 
-import signin from './app/signin'; 
+import home from './home'; 
+import signin from './signin'; 
 
 const Stack = createStackNavigator();
 
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={index} />
+        <Stack.Screen name="Home" component={home} />
         <Stack.Screen name="SignIn" component={signin} />
       </Stack.Navigator>
     </NavigationContainer>
